50adda543754982dc3efbf4da892ac50ba08265a
Faizan shaikh <faizan.shaikh@regur.net>
2025-07-26 17:03:39 +0530

    Remove build from tracking

--- build/regur-also-read-post/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '287128f2a65aecd00f0a');
--- build/regur-also-read-post/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'a7bd6582a0d938d0639f');
