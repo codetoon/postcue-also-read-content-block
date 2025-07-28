f8911382bb41c0a6dee30119fa75ea860ac7bdba
Faizan shaikh <faizan.shaikh@regur.net>
2025-07-28 18:43:01 +0530

    Updated layout of search bar with functionlity of search

--- src/regur-also-read-post/edit.js
+++ src/regur-also-read-post/edit.js
@@ -29,33 +29,62 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
-	return (
-		// returns input for search posts
-		<div {...useBlockProps()}>
-			<p>
-				{__('Search Also Read Post', 'regur-also-read-post')}
-			</p>
-			<div className='regur-also-read-post-search'>
-				<input
-				type="text"
-				onInput={(event) => {
-					handleInputChange(event.target.value);
-				}}
-				id="regur-also-read-post-search"
-				name="regur_also_read_post_search"
-				placeholder={__('Search posts', 'regur-also-read-post')}
-				className="regur-also-read-post-search-input"
-			/>
-			<button type='search' className="regur-also-read-post-search-button" onClick={(event) => {console.log('Button clicked!'); event.preventDefault();}}>
-				{__('Search', 'regur-also-read-post')}
-			</button>
-			</div>
-		</div>
-	);
+
+const postsData = [
+	{ id: 1, title: 'Top WordPress Development Companies in India: How to Choose the Right Partner?', link: '#' },
+	{ id: 2, title: 'How to Build a Scalable Software Business Starting with an MVP?', link: '#' },
+]
+
+var filteredPosts = []; // Initialize with an empty array
+
+const returnFilteredPosts = () => {
+	return filteredPosts;
 }
 
-// const handleInputChange = (value) => {
-// 	Post(value); // Call the Post function with the input value
-// }
+const handleInputChange = (value) => {
+	filteredPosts = postsData.filter(post => post.title.toLowerCase().includes(value.toLowerCase()));
+
+	if (value === '') {
+		filteredPosts = []; // Reset to all posts if input is empty
+	}
+
+	returnFilteredPosts()
+}
+
+export default function Edit() {
+
+	return (
+		// returns input for search posts
+
+		<div {...useBlockProps()}>
+			<div className='regur-also-read-post-search'>
+				<input
+					type="text"
+					onChange={(event) => {
+						handleInputChange(event.target.value);
+					}}
+					id="regur-also-read-post-search"
+					name="regur_also_read_post_search"
+					placeholder={__('Search posts', 'regur-also-read-post')}
+					className="regur-also-read-post-search-input"
+				/>
+			</div>
+
+			{
+				filteredPosts.length > 0 && (
+					<ul className="regur-also-read-post-list">
+						{filteredPosts.map(post => (
+							<li key={post.id} className="regur-also-read-post-item">
+								<a href={post.link} className="regur-also-read-post-link">
+									{post.title}
+								</a>
+							</li>
+						))}
+					</ul>
+				)}
+
+		</div>
+
+	);
+}
 
