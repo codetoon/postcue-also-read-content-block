160926fbe47b07827fb4ece0134beec5fb4b9b3c
Faizan shaikh <faizan.shaikh@regur.net>
2025-08-05 16:38:17 +0530

    Moved post style into global style for now and change class name

--- src/regur-also-read-post/post.js
+++ src/regur-also-read-post/post.js
@@ -1,10 +1,8 @@
-import "./post.scss";
-
 export default function Post({ selectedPost }) {
     return (
         <>
-            <h2 class="post-title">Also Read</h2>
-            <ul class="post-listing">
+            <h2 class="display-posts-title">Also Read</h2>
+            <ul class="display-posts-listing">
                 <li class="listing-item">
                     <a class="image" target="_blank" href={selectedPost.link}><img decoding="async" width="150" height="150" src={selectedPost.thumbnail} class="attachment-thumbnail size-thumbnail wp-post-image" alt="" /></a> <a class="title" target="_blank" href={selectedPost.link}>{selectedPost.title}</a>
                 </li>
