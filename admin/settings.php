42c9fa14af2cee7c8d8624b93d57461d2641b6b2
Faizan shaikh <faizan.shaikh@regur.net>
2025-09-24 12:06:06 +0530

    Moved icon into images folder

--- admin/settings.php
+++ admin/settings.php
@@ -12,7 +12,7 @@ add_action('admin_menu', function() {
             'edit_posts',
             'rts-wp-also-read-settings',
             'rtswpar_settings_page',
-            plugin_dir_url(__DIR__) . 'assets/img/icon.svg',
+            plugin_dir_url(__DIR__) . 'images/icon.svg',
             80     
         );
     }
